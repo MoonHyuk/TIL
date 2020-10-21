5c13505b3388d53aeadd452569e5cfd9199bb317
moonhyuk <moonhyuk@sellmate.co.kr>
2020-10-21 10:33:14 +0900

    exercise5 add inputs

--- css/exercises/5_profile_generator/script.js
+++ css/exercises/5_profile_generator/script.js
@@ -1,27 +1,33 @@
-const cols = 20;
-const rows = 20;
-const ratio = 0.5;
+const draw = () => {
+  const container = document.getElementsByClassName("container")[0];
+  container.innerHTML = "";
 
-const color =
-  "rgb(" +
-  Math.random() * 255 +
-  "," +
-  Math.random() * 255 +
-  "," +
-  Math.random() * 255 +
-  ")";
-
-const container = document.getElementsByClassName("container")[0];
-container.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
-container.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
-
-for (let i = 0; i < cols * rows; i++) {
-  const item = document.createElement("div");
-  item.classList.add("item");
-
-  if (Math.random() < ratio) {
-    item.style.backgroundColor = color;
-  }
-
-  container.appendChild(item);
+  const cols = document.getElementsByName('cols')[0].value;
+  const rows = document.getElementsByName('rows')[0].value;
+  const ratio = 0.5;
+  
+  const color =
+    "rgb(" +
+    Math.random() * 255 +
+    "," +
+    Math.random() * 255 +
+    "," +
+    Math.random() * 255 +
+    ")";
+  
+  container.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
+  container.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
+  
+  for (let i = 0; i < cols * rows; i++) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+  
+    if (Math.random() < ratio) {
+      item.style.backgroundColor = color;
+    }
+  
+    container.appendChild(item);
+  }  
 }
+
+draw();
