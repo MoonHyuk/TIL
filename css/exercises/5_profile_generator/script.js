ce126b10cf0463a6c605afd27fbbdb0529fff6f0
moonhyuk <moonhyuk@sellmate.co.kr>
2020-10-21 10:09:03 +0900

    [CSS] exercise5 upgrade to es6

--- css/exercises/5_profile_generator/script.js
+++ css/exercises/5_profile_generator/script.js
@@ -1,8 +1,8 @@
-var cols = 20;
-var rows = 20;
-var ratio = 0.5;
+const cols = 20;
+const rows = 20;
+const ratio = 0.5;
 
-var color =
+const color =
   "rgb(" +
   Math.random() * 255 +
   "," +
@@ -11,12 +11,12 @@ var color =
   Math.random() * 255 +
   ")";
 
-var container = document.getElementsByClassName("container")[0];
+const container = document.getElementsByClassName("container")[0];
 container.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
 container.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
 
-for (var i = 0; i < cols * rows; i++) {
-  var item = document.createElement("div");
+for (let i = 0; i < cols * rows; i++) {
+  const item = document.createElement("div");
   item.classList.add("item");
 
   if (Math.random() < ratio) {
