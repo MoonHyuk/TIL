a2b7cc9c8cefd20cd5d048d64ccb52c766bfe2e9
moonhyuk <godmoon00@naver.com>
2020-10-22 22:53:20 +0900

    [Exercise] format code

--- exercises/5_profile_generator/script.js
+++ exercises/5_profile_generator/script.js
@@ -2,10 +2,10 @@ const draw = () => {
   const container = document.getElementsByClassName("container")[0];
   container.innerHTML = "";
 
-  const cols = document.getElementsByName('cols')[0].value;
-  const rows = document.getElementsByName('rows')[0].value;
+  const cols = document.getElementsByName("cols")[0].value;
+  const rows = document.getElementsByName("rows")[0].value;
   const ratio = 0.5;
-  
+
   const color =
     "rgb(" +
     Math.random() * 255 +
@@ -14,20 +14,20 @@ const draw = () => {
     "," +
     Math.random() * 255 +
     ")";
-  
+
   container.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
   container.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
-  
+
   for (let i = 0; i < cols * rows; i++) {
     const item = document.createElement("div");
     item.classList.add("item");
-  
+
     if (Math.random() < ratio) {
       item.style.backgroundColor = color;
     }
-  
+
     container.appendChild(item);
-  }  
-}
+  }
+};
 
 draw();
