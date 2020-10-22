d5015594aeb0644385e21d41f40545549422f1c7
moonhyuk <godmoon00@naver.com>
2020-10-22 23:53:55 +0900

    [Exercise] profile-generator: make profile symmetric

--- exercises/5_profile_generator/script.js
+++ exercises/5_profile_generator/script.js
@@ -2,8 +2,19 @@ const draw = () => {
   const container = document.getElementsByClassName("container")[0];
   container.innerHTML = "";
 
-  const cols = document.getElementsByName("cols")[0].value;
-  const rows = document.getElementsByName("rows")[0].value;
+  const cols = parseInt(document.getElementsByName("cols")[0].value);
+  const rows = parseInt(document.getElementsByName("rows")[0].value);
+
+  container.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
+  container.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
+
+  for (let i = 0; i < cols * rows; i++) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    container.appendChild(item);
+  }
+
   const ratio = 0.5;
 
   const color =
@@ -15,18 +26,15 @@ const draw = () => {
     Math.random() * 255 +
     ")";
 
-  container.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
-  container.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
+  const items = document.getElementsByClassName("item");
 
-  for (let i = 0; i < cols * rows; i++) {
-    const item = document.createElement("div");
-    item.classList.add("item");
-
-    if (Math.random() < ratio) {
-      item.style.backgroundColor = color;
+  for (let col = 0; col < cols / 2; col++) {
+    for (let row = 0; row < rows; row++) {
+      if (Math.random() < ratio) {
+        items[row * cols + col].style.backgroundColor = color;
+        items[row * cols + cols - col - 1].style.backgroundColor = color;
+      }
     }
-
-    container.appendChild(item);
   }
 };
 
