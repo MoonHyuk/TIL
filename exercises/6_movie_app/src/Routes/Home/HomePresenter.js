81b98566323b587be8a1c07fdc951cf351f11755
moonhyuk <moonhyuk@sellmate.co.kr>
2020-11-01 01:02:20 +0900

    [Exercise] movie-app: add Presenters

--- exercises/6_movie_app/src/Routes/Home/HomePresenter.js
+++ exercises/6_movie_app/src/Routes/Home/HomePresenter.js
@@ -1,1 +1,49 @@
-export default () => "hi";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
+const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) =>
+  loading ? (
+    <Loader></Loader>
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  popular: PropTypes.array,
+  upcoming: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
