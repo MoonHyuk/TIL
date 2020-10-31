e27a5fa761d132da90712976f00134fa6a4ca464
moonhyuk <godmoon00@naver.com>
2020-10-31 23:00:24 +0900

    [Exercise] movie-app: learn axios

---
+++ exercises/6_movie_app/src/api.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "70185d7019a6db9f128a73723355b890",
+    language: "ko-KR",
+  },
+});
+
+export const tvApi = {
+  nowPlaying: () => api.get("tv/now_playing"),
+};
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  showDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (terms) =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(terms),
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
