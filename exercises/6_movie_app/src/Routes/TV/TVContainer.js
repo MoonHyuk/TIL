958dd46636aabf77d8909cd84c3ece7d8fb71ea3
moonhyuk <godmoon00@naver.com>
2020-11-01 00:12:22 +0900

    [Exercise] movie-app: add HomeContainer, TVContainer

--- exercises/6_movie_app/src/Routes/TV/TVContainer.js
+++ exercises/6_movie_app/src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -10,7 +11,38 @@ export default class extends React.Component {
     loading: true,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+
+      this.setState({
+        topRated,
+        airingToday,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "영화 정보를 가져오는데 실패했습니다.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   render() {
+    console.log(this.state);
     const { topRated, popular, airingToday, error, loading } = this.state;
 
     return (
